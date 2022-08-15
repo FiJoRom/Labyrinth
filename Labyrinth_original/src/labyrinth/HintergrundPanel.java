0fedba0e62385380870efcbfed29a9b8a8dc66fd
chanti01 <110824579+chanti01@users.noreply.github.com>
2022-08-15 16:19:03 +0200

    Hintergrundpanel gekuerzt

--- Labyrinth_original/src/labyrinth/HintergrundPanel.java
+++ Labyrinth_original/src/labyrinth/HintergrundPanel.java
@@ -5,12 +5,11 @@ import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class HintergrundPanel extends JPanel {
+	
+	private static final long serialVersionUID = 7066337001770813170L;
 	
 	String farbe1;
 	String farbe2;
@@ -19,24 +18,17 @@ public class HintergrundPanel extends JPanel {
 		
 		this.farbe1 = eingabeFarbe1;
 		this.farbe2 = eingabeFarbe2;
-		
 	}
-	
 	
 	@Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        
+		super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         int breite = getWidth(), hoehe = getHeight();
-        //Color color1 = Color.decode("#ff561A");
         Color color1 = Color.decode(farbe1);
         Color color2 = Color.decode(farbe2);
-       
-        //Color color2 = Color.decode("#ffd662");
-        //Color color2 = Color.decode("#ffcc40");
-        
-        //GradientPaint farbverlaufMitteObenUnten = new GradientPaint(breite/2, 0, color1, breite/2, hoehe, color2);
         
         GradientPaint farbverlaufMitteObenUnten = new GradientPaint(breite/2, 0, color1, breite/2, hoehe, color2);
         g2d.setPaint(farbverlaufMitteObenUnten);
